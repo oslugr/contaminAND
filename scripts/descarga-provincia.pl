989ec1182a7485b1d4e5e090eea7b1f0872c5633
JJ Merelo <jjmerelo@gmail.com>
2017-03-04 18:31:20 +0100

    Restaura el descargador original

--- scripts/descarga-provincia.pl
+++ scripts/descarga-provincia.pl
@@ -8,9 +8,43 @@ use Mojo::DOM;
 use LWP::Simple;
 use File::Slurp::Tiny qw(write_file);
 use JSON;
+use DateTime;
+
+
+my @provincias = qw(al ca ma gr se co hu ja);
+
+my %provincias = map { $_ => 1 } @provincias;
+
+my @meses = qw(ene feb mar abr may jun jul ago sep oct nov dic);
+
+my $provincia = shift || "gr";
+
+my $current = DateTime->new(
+    day   => 1,
+    month => 2,
+    year  => 2017,
+);
+
+my $stop = DateTime->new(
+    day   => 18,
+    month => 2,
+    year  => 2017,
+);
+
+while ( $current->add(days => 1) < $stop ) {
+    my $year = substr $current->strftime('%Y'), 2, 4;
+    my $mes = $current->strftime('%m');
+    my $dia = $current->strftime('%d');
+    my $date =  $year.$mes.$dia;
+    #printf "Descargando informacion de ngr%s\n", $date;
+
+    my $path = 'http://www.juntadeandalucia.es/medioambiente/atmosfera/informes_siva/'.$meses[$mes-1].$year.'/n$provincia'.$date.'.htm';
+    printf "URL: %s\n",$path;
+
+    $current = $current->add(days => 1) ;
+}
 
 my $url = shift || 'http://www.juntadeandalucia.es/medioambiente/atmosfera/informes_siva/feb17/ngr170201.htm';
-
 
 my $content = get( $url );
 
@@ -48,7 +82,7 @@ while ( @tables ) {
   }
   pop(@medidas);
   push @datos, { meta => $this_metadata,
-		 medidas => \@medidas };
+	     medidas => \@medidas };
 }
 
 my ($mesyear,$sitio,$dia) = ($url =~ m{/(\w+)/n(\w{2})\d{4}(\d{2})\.htm});
