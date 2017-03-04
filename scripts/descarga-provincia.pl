396b6ff85bbd124f11af4d5be3f1cf0c84a6db56
JJ Merelo <jjmerelo@gmail.com>
2017-03-04 18:59:29 +0100

    Descargando datos año 2017

--- scripts/descarga-provincia.pl
+++ scripts/descarga-provincia.pl
@@ -21,73 +21,70 @@ my $provincia = shift || "gr";
 
 my $current = DateTime->new(
     day   => 1,
-    month => 2,
+    month => 1,
     year  => 2017,
 );
 
 my $stop = DateTime->new(
-    day   => 18,
-    month => 2,
+    day   => 3,
+    month => 3,
     year  => 2017,
 );
 
-while ( $current->add(days => 1) < $stop ) {
-    my $year = substr $current->strftime('%Y'), 2, 4;
-    my $mes = $current->strftime('%m');
-    my $dia = $current->strftime('%d');
-    my $date =  $year.$mes.$dia;
-    #printf "Descargando informacion de ngr%s\n", $date;
-
-    my $path = 'http://www.juntadeandalucia.es/medioambiente/atmosfera/informes_siva/'.$meses[$mes-1].$year.'/n$provincia'.$date.'.htm';
-    printf "URL: %s\n",$path;
-
-    $current = $current->add(days => 1) ;
-}
-
-my $url = shift || 'http://www.juntadeandalucia.es/medioambiente/atmosfera/informes_siva/feb17/ngr170201.htm';
-
-my $content = get( $url );
-
-if    ( !$content )  {
-  die "No puedo descargar $url";
-}
-
-my $dom = Mojo::DOM->new( $content );
-my @tables = $dom->find('table')->each;
-
-shift @tables; #Primera tabla con leyenda
-
 my @datos;
-while ( @tables ) {
-  my $metadatos = shift @tables;
-  my $datos = shift @tables;
 
-  my @metadatos = ( $metadatos =~ /<b>.([A-Z][^<]+)/g);
-  my $this_metadata;
-  for my $k (qw(provincia municipio estacion direccion)) {
-    $this_metadata->{$k} = shift @metadatos;
-  }
-
-  my @filas = $datos->find('tr')->each;
-
-  shift @filas; #Cabecera
-  my @medidas;
-  for my $f (@filas) {
-    my @columnas = $f->find('td')->map('text')->each;
-    my $these_medidas;
-    for my $c (qw(hora SO2 PART NO2 CO O3)) {
-      $these_medidas->{$c} = shift @columnas;
+while ( $current < $stop ) {
+  my $year = substr $current->strftime('%Y'), 2, 4;
+  my $mes = $current->strftime('%m');
+  my $dia = $current->strftime('%d');
+  my $date =  $year.$mes.$dia;
+  #printf "Descargando informacion de ngr%s\n", $date;
+  
+  my $url = "http://www.juntadeandalucia.es/medioambiente/atmosfera/informes_siva/$meses[$mes-1]$year/n$provincia$date.htm";
+  
+  my $content = get( $url );
+  
+  if    ( $content )  {
+    my $dom = Mojo::DOM->new( $content );
+    my @tables = $dom->find('table')->each;
+    
+    shift @tables; #Primera tabla con leyenda
+    
+    my $fecha = "$current";
+    
+    while ( @tables ) {
+      my $metadatos = shift @tables;
+      my $datos = shift @tables;
+      
+      my @metadatos = ( $metadatos =~ /<b>.([A-Z][^<]+)/g);
+      my $this_metadata = { date => $fecha };
+      for my $k (qw(provincia municipio estacion direccion)) {
+	$this_metadata->{$k} = shift @metadatos;
+      }
+      
+      my @filas = $datos->find('tr')->each;
+      
+      shift @filas; #Cabecera
+      pop @filas;
+      for my $f (@filas) {
+	my @columnas = $f->find('td')->map('text')->each;
+	my %these_medidas = %{$this_metadata};
+	my $fecha_hora = shift @columnas;
+	my ($hora) = ($fecha_hora =~ /(\d+:\d+)/);
+	$these_medidas{'date'} =~ s/00:00/$hora/;
+	for my $c (qw(SO2 PART NO2 CO O3)) {
+	  $these_medidas{$c} = shift @columnas;
+	}
+	push @datos, \%these_medidas;
+      }
     }
-    push @medidas, $these_medidas;
   }
-  pop(@medidas);
-  push @datos, { meta => $this_metadata,
-	     medidas => \@medidas };
+  
+  $current = $current->add(days => 1) ;
 }
 
-my ($mesyear,$sitio,$dia) = ($url =~ m{/(\w+)/n(\w{2})\d{4}(\d{2})\.htm});
 
-write_file( "contaminAND-$sitio-$mesyear-$dia.json", encode_json \@datos );
+write_file( "contaminAND-$provincia.json", encode_json \@datos );
 
 
 			     
