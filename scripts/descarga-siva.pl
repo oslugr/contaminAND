fb60cbbcd848c5a8b3a119d5f65d6a3acd0d78c2
Xisco Fauli <anistenis@gmail.com>
2017-03-04 18:18:41 +0100

    [WIP] Primer intento para generar la URL entre dos fechas

--- scripts/descarga-siva.pl
+++ scripts/descarga-siva.pl
@@ -8,9 +8,48 @@ use Mojo::DOM;
 use LWP::Simple;
 use File::Slurp::Tiny qw(write_file);
 use JSON;
+use DateTime;
+
+my $start = DateTime->new(
+    day   => 1,
+    month => 2,
+    year  => 2017,
+);
+
+my $stop = DateTime->new(
+    day   => 18,
+    month => 2,
+    year  => 2017,
+);
+
+my %sMes = (
+    '01' => "ene",
+    '02' => "feb",
+    '03' => "mar",
+    '04' => "abr",
+    '05' => "may",
+    '06' => "jun",
+    '07' => "jul",
+    '08' => "ago",
+    '09' => "sep",
+    '10' => "oct",
+    '11' => "nov",
+    '12' => "dic",
+);
+
+while ( $start->add(days => 1) < $stop ) {
+    my $year = substr $start->strftime('%Y'), 2, 4;
+    my $mes = $start->strftime('%m');
+    my $dia = $start->strftime('%d');
+    my $date =  $year.$mes.$dia;
+    #printf "Descargando informacion de ngr%s\n", $date;
+
+    my $path = 'http://www.juntadeandalucia.es/medioambiente/atmosfera/informes_siva/'.$sMes{$mes}.$year.'/ngr'.$date.'.htm';
+    printf "URL: %s\n",$path;
+
+}
 
 my $url = shift || 'http://www.juntadeandalucia.es/medioambiente/atmosfera/informes_siva/feb17/ngr170201.htm';
-
 
 my $content = get( $url );
 
@@ -48,7 +87,7 @@ while ( @tables ) {
   }
   pop(@medidas);
   push @datos, { meta => $this_metadata,
-		 medidas => \@medidas };
+	     medidas => \@medidas };
 }
 
 my ($mesyear,$sitio,$dia) = ($url =~ m{/(\w+)/n(\w{2})\d{4}(\d{2})\.htm});
